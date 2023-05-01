f484b508b1c38a3be13205ec3059899518b9381e
stl <stl@peleion.local>
2023-05-02 08:44:47 +0900

    update librespot to 0.4.2

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,13 @@ extern crate librespot_metadata;
 extern crate log;
 extern crate regex;
 extern crate scoped_threadpool;
-extern crate tokio_core;
+extern crate tokio;
 
 use std::env;
 use std::io::{self, BufRead, Read, Result};
 use std::io::Write;
 use std::path::Path;
 use std::process::{Command, Stdio};
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::time::Duration;
 
 use env_logger::{Builder, Env};
 use librespot_audio::{AudioDecrypt, AudioFile};
@@ -25,7 +23,7 @@ use librespot_core::spotify_id::SpotifyId;
 use librespot_metadata::{Artist, FileFormat, Metadata, Track, Album};
 use regex::Regex;
 use scoped_threadpool::Pool;
-use tokio_core::reactor::Core;
+use tokio::runtime::Runtime;
 
 fn main() {
     Builder::from_env(Env::default().default_filter_or("info")).init();
@@ -33,14 +31,13 @@ fn main() {
     let args: Vec<_> = env::args().collect();
     assert!(args.len() == 3 || args.len() == 4, "Usage: {} user password [helper_script] < tracks_file", args[0]);
 
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
+    let core = Runtime::new().unwrap();
     let session_config = SessionConfig::default();
     let credentials = Credentials::with_password(args[1].to_owned(), args[2].to_owned());
     info!("Connecting ...");
     let session = core
-        .run(Session::connect(session_config, credentials, None, handle))
-        .unwrap();
+        .block_on(Session::connect(session_config, credentials, None, false))
+        .unwrap().0;
     info!("Connected!");
 
     let mut threadpool = Pool::new(1);
@@ -57,16 +54,16 @@ fn main() {
                     .or_else(|| { warn!("Cannot parse track from string {}", str); None })
                     .and_then(|capture|SpotifyId::from_base62(&capture[1]).ok())))
         .for_each(|id|{
-            info!("Getting track {}...", id.to_base62());
-            let mut track = core.run(Track::get(&session, id)).expect("Cannot get track metadata");
-            let orig_track = core.run(Track::get(&session, id)).expect("Cannot get track metadata");
+            info!("Getting track {}...", id.to_base62().expect("UTF8 error"));
+            let mut track = core.block_on(Track::get(&session, id)).expect("Cannot get track metadata");
+            let orig_track = core.block_on(Track::get(&session, id)).expect("Cannot get track metadata");
             if !track.available {
-                warn!("Track {} is not available, finding alternative...", id.to_base62());
+                warn!("Track {} is not available, finding alternative...", id.to_base62().expect("UTF8 error"));
                 let alt_track = track.alternatives.iter().find_map(|id|{
-                    let alt_track = core.run(Track::get(&session, *id)).expect("Cannot get track metadata");
+                    let alt_track = core.block_on(Track::get(&session, *id)).expect("Cannot get track metadata");
                     match alt_track.available {
                         true => {
-                            if alt_track.id.to_base62() == track.id.to_base62() {
+                            if alt_track.id.to_base62().expect("UTF8 error") == track.id.to_base62().expect("UTF8 error") {
                                 //warn!("ALTERNATE ID SAME");
                                 return None;
                             }
@@ -77,16 +74,16 @@ fn main() {
                 });
                 match alt_track {
                     Some(alt_track) => {
-                        warn!("Found track alternative {} -> {}", id.to_base62(), alt_track.id.to_base62());
+                        warn!("Found track alternative {} -> {}", id.to_base62().expect("UTF8 error"), alt_track.id.to_base62().expect("UTF8 error"));
                         track = alt_track;
                     }
                     None => {
-                        warn!("Could not find alternative for track {}", id.to_base62());
+                        warn!("Could not find alternative for track {}", id.to_base62().expect("UTF8 error"));
                         return;
                     }
                  }
             }
-            let artists_strs: Vec<_> = orig_track.artists.iter().map(|id|core.run(Artist::get(&session, *id)).expect("Cannot get artist metadata").name).collect();
+            let artists_strs: Vec<_> = orig_track.artists.iter().map(|id|core.block_on(Artist::get(&session, *id)).expect("Cannot get artist metadata").name).collect();
 
 
 
@@ -94,13 +91,13 @@ fn main() {
 
 
 
-            let album = core.run(Album::get(&session, orig_track.album)).expect("Cannot get album metadata");
+            let album = core.block_on(Album::get(&session, orig_track.album)).expect("Cannot get album metadata");
 
             // from
             // https://stackoverflow.com/questions/38461429/how-can-i-truncate-a-string-to-have-at-most-n-characters
 
             let max_width = 255-4;
-            let fname_minus_extension = format!("{} --- {} --- {} --- {}", id.to_base62(), artists_strs.join(", "), orig_track.name, album.name)
+            let fname_minus_extension = format!("{} --- {} --- {} --- {}", id.to_base62().expect("UTF8 error"), artists_strs.join(", "), orig_track.name, album.name)
                 .chars()
                 .take(max_width)
                 .collect::<String>()
@@ -122,19 +119,14 @@ fn main() {
                 .or(track.files.get(&FileFormat::OGG_VORBIS_160))
                 .or(track.files.get(&FileFormat::OGG_VORBIS_96))
                 .expect("Could not find a OGG_VORBIS format for the track.");
-            let key = core.run(session.audio_key().request(track.id, *file_id)).expect("Cannot get audio key");
-            let mut encrypted_file = core.run(AudioFile::open(&session, *file_id, 320, true)).unwrap();
+            let key = core.block_on(session.audio_key().request(track.id, *file_id)).expect("Cannot get audio key");
+            let mut encrypted_file = core.block_on(AudioFile::open(&session, *file_id, 320, true)).unwrap();
             let mut buffer = Vec::new();
             let mut read_all: Result<usize> = Ok(0);
-            let fetched = AtomicBool::new(false);
             threadpool.scoped(|scope|{
                 scope.execute(||{
                     read_all = encrypted_file.read_to_end(&mut buffer);
-                    fetched.store(true, Ordering::Release);
                 });
-                while !fetched.load(Ordering::Acquire) {
-                    core.turn(Some(Duration::from_millis(100)));
-                }
             });
             read_all.expect("Cannot read file stream");
             let mut decrypted_buffer = Vec::new();
@@ -145,8 +137,8 @@ fn main() {
             } else {
                 let mut cmd = Command::new(args[3].to_owned());
                 cmd.stdin(Stdio::piped());
-                cmd.arg(id.to_base62()).arg(orig_track.name).arg(album.name).args(artists_strs.iter());
-                let mut child = cmd.spawn().expect("Could not run helper program");
+                cmd.arg(id.to_base62().expect("UTF8 error")).arg(orig_track.name).arg(album.name).args(artists_strs.iter());
+                let mut child = cmd.spawn().expect("Could not block_on helper program");
                 let pipe = child.stdin.as_mut().expect("Could not open helper stdin");
                 pipe.write_all(&decrypted_buffer[0xa7..]).expect("Failed to write to stdin");
                 assert!(child.wait().expect("Out of ideas for error messages").success(), "Helper script returned an error");
